55a429df688f0d0a4613a7e42d8f3bb24313bab5
Piotr Pacewicz <piotrp92@gmail.com>
2019-12-02 19:37:18 +0000

    fix a bug with animation

--- animation.js
+++ animation.js
@@ -1,63 +1,67 @@
 /*HEADER*/
 
-
-ScrollReveal().reveal('.logo', {
-    duration: 1000,
-    distance: '30px',
-    origin: 'top',
-    delay: 350
+ScrollReveal().reveal(".logo", {
+  //   duration: 1000,
+  distance: "30px",
+  origin: "top",
+  delay: 350
 });
-ScrollReveal().reveal('.logo__mobile', {
-    delay: 350
-});
-
-ScrollReveal().reveal('.nav-item', {
-    interval: 200,
+ScrollReveal().reveal(".logo__mobile", {
+  //   duration: 1000,
+  distance: "30px",
+  origin: "top",
+  delay: 350
 });
 
-ScrollReveal().reveal('.header__icon', {
-    distance: '60px',
-    origin: 'right',
-    interval: 200
+ScrollReveal().reveal(".nav-item", {
+  interval: 200
+});
+
+ScrollReveal().reveal(".header__icon", {
+  distance: "60px",
+  origin: "top",
+  interval: 200
 });
 
 /*ABOUT ME*/
-ScrollReveal().reveal('.aboutme', {
-    distance: '60px',
-    origin: 'left',
-    delay: 350
+ScrollReveal().reveal(".aboutme", {
+  //   duration: 1000,
+  distance: "60px",
+  origin: "bottom",
+  delay: 350
 });
 
-ScrollReveal().reveal('.profile__photo', {
-    distance: '60px',
-    origin: 'right',
-    delay: 350
+ScrollReveal().reveal(".profile__photo", {
+  //   duration: 1000,
+  distance: "60px",
+  origin: "top",
+  delay: 350
 });
-
 
 /*SKILLS*/
 
-
-ScrollReveal().reveal('.skill__item', {
-    interval: 300
+ScrollReveal().reveal(".skill__item", {
+  interval: 300
 });
 
 /*PROJECT*/
 
-ScrollReveal().reveal('.first__project', {
-    distance: '60px',
-    origin: 'left',
-    delay: 750,
+ScrollReveal().reveal(".first__project", {
+  //   duration: 1000,
+  distance: "60px",
+  origin: "top",
+  delay: 750
 });
-ScrollReveal().reveal('.second__project', {
-    distance: '60px',
-    origin: 'right',
-    delay: 1000
+ScrollReveal().reveal(".second__project", {
+  //   duration: 1000,
+  distance: "60px",
+  origin: "bottom",
+  delay: 750
 });
 
 /*CONTACT*/
-ScrollReveal().reveal('.contact__icon', {
-    distance: '60px',
-    origin: 'left',
-    interval: 300
+ScrollReveal().reveal(".contact__icon", {
+  distance: "60px",
+  origin: "left",
+  interval: 300
 });
