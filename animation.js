27558cb0255995fd86236a620b7c8175013d6ade
Piotr Pacewicz <piotrp92@gmail.com>
2019-12-02 19:07:53 +0000

    add animation to all sections

---
+++ animation.js
@@ -0,0 +1,63 @@
+/*HEADER*/
+
+
+ScrollReveal().reveal('.logo', {
+    duration: 1000,
+    distance: '30px',
+    origin: 'top',
+    delay: 350
+});
+ScrollReveal().reveal('.logo__mobile', {
+    delay: 350
+});
+
+ScrollReveal().reveal('.nav-item', {
+    interval: 200,
+});
+
+ScrollReveal().reveal('.header__icon', {
+    distance: '60px',
+    origin: 'right',
+    interval: 200
+});
+
+/*ABOUT ME*/
+ScrollReveal().reveal('.aboutme', {
+    distance: '60px',
+    origin: 'left',
+    delay: 350
+});
+
+ScrollReveal().reveal('.profile__photo', {
+    distance: '60px',
+    origin: 'right',
+    delay: 350
+});
+
+
+/*SKILLS*/
+
+
+ScrollReveal().reveal('.skill__item', {
+    interval: 300
+});
+
+/*PROJECT*/
+
+ScrollReveal().reveal('.first__project', {
+    distance: '60px',
+    origin: 'left',
+    delay: 750,
+});
+ScrollReveal().reveal('.second__project', {
+    distance: '60px',
+    origin: 'right',
+    delay: 1000
+});
+
+/*CONTACT*/
+ScrollReveal().reveal('.contact__icon', {
+    distance: '60px',
+    origin: 'left',
+    interval: 300
+});
